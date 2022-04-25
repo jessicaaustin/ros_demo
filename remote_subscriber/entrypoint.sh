ea37efb016cd49bcc74cb74e8b26b73f50f52eca
Jessica Austin <jessica@bedrockocean.com>
2022-04-25 11:36:16 -0800

    add debug logging for python

--- remote_subscriber/entrypoint.sh
+++ remote_subscriber/entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /usr/src/catkin_ws/devel/setup.bash
 
+# Python node config
+export ROS_PYTHON_LOG_CONFIG_FILE=/usr/src/python_logging.conf
+
 exec "$@"
 
